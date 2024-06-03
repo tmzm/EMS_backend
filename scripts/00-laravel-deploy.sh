da9b6b0750ac50c65a5fec0ac3f4cacf9b5a1907
tareq mozayek <tareqmozayek@gmail.com>
2024-06-03 22:06:28 +0300

    docker migration 16

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,25 +1,25 @@
-#!/usr/bin/env bash
-echo "Running composer"
-composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
-
-echo "generating application key..."
-php artisan key:generate --show
-
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
-
-echo "Running migrations..."
-php artisan migrate --force
-
-echo "Running seed..."
-php artisan db:seed
-
-echo "Running passport..."
-php artisan passport:client --personal
-
-echo "Running app..."
-php artisan serve
+##!/usr/bin/env bash
+#echo "Running composer"
+#composer global require hirak/prestissimo
+#composer install --no-dev --working-dir=/var/www/html
+#
+#echo "generating application key..."
+#php artisan key:generate --show
+#
+#echo "Caching config..."
+#php artisan config:cache
+#
+#echo "Caching routes..."
+#php artisan route:cache
+#
+#echo "Running migrations..."
+#php artisan migrate --force
+#
+#echo "Running seed..."
+#php artisan db:seed
+#
+#echo "Running passport..."
+#php artisan passport:client --personal
+#
+#echo "Running app..."
+#php artisan serve
