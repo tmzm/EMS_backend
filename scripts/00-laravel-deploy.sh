a30309bc2c51d35437c3ae12e67058d3d2ab382b
tareq mozayek <tareqmozayek@gmail.com>
2024-06-03 21:52:32 +0300

    docker migration 12

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,11 +1,11 @@
-##!/usr/bin/env bash
-#echo "Running composer"
-#composer global require hirak/prestissimo
-#composer install --no-dev --working-dir=/var/www/html
-#
-#echo "generating application key..."
-#php artisan key:generate --show
-#
+#!/usr/bin/env bash
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+
+echo "generating application key..."
+php artisan key:generate --show
+
 echo "Caching config..."
 php artisan config:cache
 
