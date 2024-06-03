b41c1b5985f2fe9aca78642f322ca132fbcb855b
tareq mozayek <tareqmozayek@gmail.com>
2024-06-03 21:49:48 +0300

    docker migration 10

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -6,20 +6,20 @@
 #echo "generating application key..."
 #php artisan key:generate --show
 #
-#echo "Caching config..."
-#php artisan config:cache
-#
-#echo "Caching routes..."
-#php artisan route:cache
-#
-#echo "Running migrations..."
-#php artisan migrate --force
-#
-#echo "Running seed..."
-#php artisan db:seed
-#
-#echo "Running app..."
-#php artisan serve --host 0.0.0.0 --port 10000
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Running seed..."
+php artisan db:seed
+
+echo "Running app..."
+php artisan serve --host 0.0.0.0 --port 10000
 
 echo "Running passport..."
 php passport client --personal
